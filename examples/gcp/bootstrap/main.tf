dbdf60e6d2b3c290c3f6311fa5e74c5c60a6a9b0
bodymindarts <justin@misthos.io>
2021-06-29 16:10:35 +0200

    Update examples

--- examples/gcp/bootstrap/main.tf
+++ examples/gcp/bootstrap/main.tf
@@ -2,7 +2,7 @@ variable "name_prefix" {}
 variable "gcp_project" {}
 
 module "bootstrap" {
-  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/bootstrap/gcp?ref=f19ab4e"
+  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/bootstrap/gcp?ref=b397ae8"
 
   name_prefix = var.name_prefix
   gcp_project = var.gcp_project
