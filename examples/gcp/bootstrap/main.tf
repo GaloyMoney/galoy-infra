0e0cbe0751710c3ab0562bdd71264785a454bae3
bodymindarts <justin@misthos.io>
2021-06-29 16:15:23 +0200

    Bump examples

--- examples/gcp/bootstrap/main.tf
+++ examples/gcp/bootstrap/main.tf
@@ -1,9 +1,13 @@
 variable "name_prefix" {}
 variable "gcp_project" {}
+variable "enable_services" {
+  default = true
+}
 
 module "bootstrap" {
-  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/bootstrap/gcp?ref=b397ae8"
+  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/bootstrap/gcp?ref=4cbc6b7"
 
   name_prefix = var.name_prefix
   gcp_project = var.gcp_project
+  enable_services = enable_services
 }
