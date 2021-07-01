ff12d374e857f6d4dac0b69247f8616fbd933caf
bodymindarts <justin@misthos.io>
2021-07-01 11:05:15 +0200

    GOOGLE_CREDENTIALS for inception

--- ci/tasks/teardown.sh
+++ ci/tasks/teardown.sh
@@ -13,9 +13,8 @@ init_kubeconfig
 init_bootstrap
 
 bin/prep-inception.sh
-export GOOGLE_CREDENTIALS=$(cat inception-sa-creds.json)
 
-echo yes | make destroy-inception
+echo yes | GOOGLE_CREDENTIALS=$(cat inception-sa-creds.json) make destroy-inception
 echo yes | TF_VAR_tf_state_bucket_force_destroy=true make destroy-bootstrap
 
 make_commit "Bump modules to '${MODULES_GIT_REF}' in examples"
