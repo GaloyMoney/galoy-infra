a1c8650130443c3b8293accbfb6a41d56961950f
bodymindarts <justin@misthos.io>
2021-07-01 10:06:16 +0200

    Trigger

--- modules/inception/gcp/tf-state-bucket.tf
+++ modules/inception/gcp/tf-state-bucket.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "tf_state" {
   name     = "${local.name_prefix}-tf-state"
   project  = local.project
   location = local.tf_state_bucket_location
+
   versioning {
     enabled = true
   }
