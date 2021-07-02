479035fecc30d3c787b73b0a1e920edac09e8f60
bodymindarts <justin@misthos.io>
2021-07-02 13:40:17 +0200

    Silent output on scp command

--- ci/tasks/platform.sh
+++ ci/tasks/platform.sh
@@ -24,7 +24,10 @@ popd
 
 gcloud compute ssh --zone ${bastion_zone} ${bastion_name} --ssh-key-file ${CI_ROOT}/login.ssh \
   --command "ls ${CI_ROOT_DIR} || mkdir ${CI_ROOT_DIR}"
+
+echo "Copying repo to bastion"
 gcloud compute scp --recurse --zone ${bastion_zone} --ssh-key-file ${CI_ROOT}/login.ssh \
-  ./repo "${bastion_name}:${CI_ROOT_DIR}"
+  ./repo "${bastion_name}:${CI_ROOT_DIR}" > /dev/null
+echo "Copying pipeline-tasks to bastion"
 gcloud compute scp --recurse --zone ${bastion_zone} --ssh-key-file ${CI_ROOT}/login.ssh \
-  pipeline-tasks "${bastion_name}:${CI_ROOT_DIR}"
+  pipeline-tasks "${bastion_name}:${CI_ROOT_DIR}" > /dev/null
