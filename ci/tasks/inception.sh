997904fe702827d29c84df120a9385e77cb12a7e
bodymindarts <justin@misthos.io>
2021-07-02 09:33:28 +0200

    Add inception job

---
+++ ci/tasks/inception.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+source pipeline-tasks/ci/tasks/helpers.sh
+
+pushd repo/examples/gcp
+
+update_examples_git_ref
+
+init_gcloud
+init_kubeconfig
+init_bootstrap
+
+write_users
+
+bin/prep-inception.sh
+
+echo yes | GOOGLE_CREDENTIALS=$(cat inception-sa-creds.json) make inception
+
+cleanup_inception_key
