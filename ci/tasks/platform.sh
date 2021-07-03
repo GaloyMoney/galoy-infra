aa68feea6cfa8f47246431a88215e7de04b5b42c
bodymindarts <justin@misthos.io>
2021-07-03 22:07:36 +0200

    Fix platform.sh

--- ci/tasks/platform.sh
+++ ci/tasks/platform.sh
@@ -24,7 +24,7 @@ export ADDITIONAL_SSH_OPTS="-o StrictHostKeyChecking=no -i ${CI_ROOT}/login.ssh"
 set +e
 for i in {1..60}; do
   echo "Attempt ${i} to ssh to bastion"
-  ssh ${ADDITIONAL_SSH_OPTS} ${BASTION_USER}@${bastion_ip} "curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && apt-add-repository \"deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" && apt-get update && apt-get install -y make terraform" && break
+  ssh ${ADDITIONAL_SSH_OPTS} ${BASTION_USER}@${bastion_ip} 'curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - && apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" && apt-get update && apt-get install -y make terraform' && break
   sleep 2
 done
 set -e
