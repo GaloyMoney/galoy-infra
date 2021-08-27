108172aa1615cf8d59d7577a1fa9106711acf2c3
bodymindarts <justin@galoy.io>
2021-08-27 10:00:01 +0200

    Fix whitespace

--- modules/platform/gcp/variables.tf
+++ modules/platform/gcp/variables.tf
@@ -7,7 +7,7 @@ variable "network_prefix" {
   default = "10.1"
 }
 variable "kube_version" {
-  default = " 1.19.12-gke.2100"
+  default = "1.19.12-gke.2100"
 }
 variable "node_default_machine_type" {
   default = "e2-standard-2"
