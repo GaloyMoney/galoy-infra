5f6e88ee3f7cc209feabf7d90f942d8d01f78977
bodymindarts <justin@galoy.io>
2021-09-24 20:15:28 +0200

    Add namespace to kubeconfig

--- modules/services/variables.tf
+++ modules/services/variables.tf
@@ -24,6 +24,7 @@ locals {
 output "smoketest_kubeconfig" {
   value = base64encode(templatefile("${path.module}/kubeconfig.tmpl.yml",
     { name : "smoketest",
+      namespace: local.smoketest_namespace,
       cert : local.cluster_ca_cert,
       endpoint : local.cluster_endpoint,
       token = data.kubernetes_secret.smoketest_token.data.token
