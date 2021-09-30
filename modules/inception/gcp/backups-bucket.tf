8f9ec75dacf6a07379bfb3f913466c560192cfa3
bodymindarts <justin@galoy.io>
2021-09-30 15:46:11 +0200

    Rename bucket_iam_polixy

--- modules/inception/gcp/backups-bucket.tf
+++ modules/inception/gcp/backups-bucket.tf
@@ -29,7 +29,7 @@ data "google_iam_policy" "backups_access" {
   }
 }
 
-resource "google_storage_bucket_iam_policy" "policy" {
+resource "google_storage_bucket_iam_policy" "backups" {
   bucket      = google_storage_bucket.backups.name
   policy_data = data.google_iam_policy.backups_access.policy_data
 }
