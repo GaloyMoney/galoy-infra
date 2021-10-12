6843bacc3817fe5d82f70ab6968370f23770de26
bodymindarts <justin@galoy.io>
2021-10-12 10:57:37 +0200

    Upgrade kubernetes to 1.21.3

--- modules/platform/gcp/variables.tf
+++ modules/platform/gcp/variables.tf
@@ -7,7 +7,7 @@ variable "network_prefix" {
   default = "10.1"
 }
 variable "kube_version" {
-  default = "1.19.13-gke.1200"
+  default = "1.21.3-gke.2001"
 }
 variable "node_default_machine_type" {
   default = "n2-standard-2"
