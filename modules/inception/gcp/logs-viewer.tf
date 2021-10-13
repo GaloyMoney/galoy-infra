69a26520a133b1faf49dee370e4e632e1cad9631
bodymindarts <justin@galoy.io>
2021-10-13 16:00:21 +0200

    Logs viewer receives IDs

--- modules/inception/gcp/logs-viewer.tf
+++ modules/inception/gcp/logs-viewer.tf
@@ -2,6 +2,6 @@ resource "google_project_iam_member" "dev_read_log" {
   project  = local.project
   for_each = toset(local.log_viewers)
   role     = "roles/logging.viewer"
-  member   = "user:${each.key}"
+  member   = each.key
 }
 
