0b8a287ae3a8ee768c38c91412cec7aab26c5780
bodymindarts <justin@galoy.io>
2021-10-15 23:31:54 +0200

    Set GOOGLE_APPLICATION_CREDENTIALS for teardown

--- ci/tasks/teardown.sh
+++ ci/tasks/teardown.sh
@@ -20,7 +20,7 @@ bastion_ip="$(cd inception && terraform output bastion_ip | jq -r)"
 export BASTION_USER="sa_$(cat ${CI_ROOT}/gcloud-creds.json  | jq -r '.client_id')"
 export ADDITIONAL_SSH_OPTS="-o StrictHostKeyChecking=no -i ${CI_ROOT}/login.ssh"
 
-ssh ${ADDITIONAL_SSH_OPTS} ${BASTION_USER}@${bastion_ip} "cd repo/examples/gcp; echo yes | make destroy-platform"
+ssh ${ADDITIONAL_SSH_OPTS} ${BASTION_USER}@${bastion_ip} "cd repo/examples/gcp; export GOOGLE_APPLICATION_CREDENTIALS=\$(pwd)/gcloud-creds.json; echo yes | make destroy-platform"
 
 echo yes | GOOGLE_CREDENTIALS=$(cat inception-sa-creds.json) make destroy-inception
 echo yes | TF_VAR_tf_state_bucket_force_destroy=true make destroy-bootstrap
