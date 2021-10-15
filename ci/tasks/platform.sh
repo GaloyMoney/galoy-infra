7d8251e01914222ff8ea09728b43fa42ac6841a0
bodymindarts <justin@galoy.io>
2021-10-15 21:59:39 +0200

    Use GOOGLE_APPLICATION_CREDENTIALS in tasks/platform.sh

--- ci/tasks/platform.sh
+++ ci/tasks/platform.sh
@@ -42,4 +42,4 @@ cp ${CI_ROOT}/gcloud-creds.json ./
 
 bin/prep-bastion.sh
 
-ssh ${ADDITIONAL_SSH_OPTS} ${BASTION_USER}@${bastion_ip} "cd repo/examples/gcp; gcloud auth activate-service-account --key-file ./gcloud-creds.json; echo yes | make initial-platform && echo yes | make platform"
+ssh ${ADDITIONAL_SSH_OPTS} ${BASTION_USER}@${bastion_ip} "cd repo/examples/gcp; export GOOGLE_APPLICATION_CREDENTIALS=\$(pwd)/gcloud-creds.json; echo yes | make initial-platform && echo yes | make platform"
