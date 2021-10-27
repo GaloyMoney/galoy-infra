544ea86bcdd2b32e680bbbd8ce071b009d7eb968
bodymindarts <justin@galoy.io>
2021-10-27 20:40:13 +0200

    Remove node size in examples

--- examples/gcp/platform/main.tf
+++ examples/gcp/platform/main.tf
@@ -1,9 +1,6 @@
 variable "name_prefix" {}
 variable "gcp_project" {}
 variable "node_service_account" {}
-variable "node_default_machine_type" {
-  default = "e2-standard-2"
-}
 variable "letsencrypt_issuer_email" {}
 
 module "platform" {
@@ -13,7 +10,6 @@ module "platform" {
   name_prefix               = var.name_prefix
   gcp_project               = var.gcp_project
   node_service_account      = var.node_service_account
-  node_default_machine_type = var.node_default_machine_type
 }
 
 
