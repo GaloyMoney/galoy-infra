fbafa3f7bd83578753a780c8bf19250e1a5e907b
bodymindarts <justin@galoy.io>
2021-11-05 10:52:46 +0100

    Bump kube version

--- modules/platform/gcp/variables.tf
+++ modules/platform/gcp/variables.tf
@@ -7,7 +7,7 @@ variable "network_prefix" {
   default = "10.1"
 }
 variable "kube_version" {
-  default = "1.21.3-gke.2001"
+  default = "1.21.3-gke.2003"
 }
 variable "node_default_machine_type" {
   default = "n2-standard-4"
