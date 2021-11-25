ac61cfcee5ae541686725499f936a1a85a5e0533
Sandipan Dey <hey@sandipan.dev>
2021-11-26 01:37:59 +0530

    Fixing root_user and name_prefix

--- modules/services/databases.tf
+++ modules/services/databases.tf
@@ -1,6 +1,8 @@
 data "google_sql_database_instance" "postgres" {
-  name = local.postgres_instance_name
+  name    = local.postgres_instance_name
+  project = local.project
 }
+
 resource "random_password" "sql_root_user_password" {
   length  = 20
   special = false
@@ -14,7 +16,7 @@ resource "google_sql_user" "root_user" {
 
 resource "kubernetes_namespace" "pg_access" {
   metadata {
-    name = "${local.prefix}-pg-access"
+    name = "${local.name_prefix}-pg-access"
   }
 }
 
@@ -24,8 +26,8 @@ module "dealer_db" {
   prefix      = "dealer"
   owner       = google_sql_user.root_user.name
   pg_host     = data.google_sql_database_instance.postgres.private_ip_address
-  pg_username = google_sql_user.master_user.name
-  pg_password = google_sql_user.master_user.password
+  pg_username = google_sql_user.root_user.name
+  pg_password = google_sql_user.root_user.password
 }
 
 resource "kubernetes_secret" "dealer_db_credentials" {
@@ -37,6 +39,6 @@ resource "kubernetes_secret" "dealer_db_credentials" {
   data = {
     username = module.dealer_db.username
     password = module.dealer_db.password
-    host     = google_sql_database_instance.postgres.private_ip_address
+    host     = data.google_sql_database_instance.postgres.private_ip_address
   }
 }
