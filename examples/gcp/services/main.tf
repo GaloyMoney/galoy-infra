0ad4b663a5e8af23801be7a002916dc6a4287f59
bodymindarts <justin@galoy.io>
2021-11-26 10:17:05 +0100

    WIP

--- examples/gcp/services/main.tf
+++ examples/gcp/services/main.tf
@@ -1,29 +1,16 @@
 variable "name_prefix" {}
 variable "gcp_project" {}
-variable "node_service_account" {}
-variable "node_default_machine_type" {
-  default = "e2-medium"
-}
-variable "letsencrypt_issuer_email" {
-  default = "bot@galoy.io"
-}
-variable "destroyable_postgres" {
-  default = true
-}
-
-module "platform" {
-  # source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/platform/gcp?ref=094e04f"
-  source = "../../../modules/platform/gcp"
-
-  name_prefix               = var.name_prefix
-  gcp_project               = var.gcp_project
-  node_service_account      = var.node_service_account
-  node_default_machine_type = var.node_default_machine_type
-  destroyable_postgres      = var.destroyable_postgres
-}
+variable "cluster_name" {}
+variable "cluster_locatin" {}
 
 data "google_client_config" "default" {
   provider = google-beta
+}
+
+data "google_container_cluster" "primary" {
+  project  = var.gcp_project
+  name     = module.shared.cluster_name
+  location = module.shared.gcp_region
 }
 
 provider "kubernetes" {
@@ -31,16 +18,16 @@ provider "kubernetes" {
     manifest_resource = true
   }
 
-  host                   = module.platform.master_endpoint
+  host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = module.platform.cluster_ca_cert
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.platform.master_endpoint
+    host                   = "https://${data.google_container_cluster.primary.private_cluster_config.0.private_endpoint}"
     token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = module.platform.cluster_ca_cert
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   }
 }
 
