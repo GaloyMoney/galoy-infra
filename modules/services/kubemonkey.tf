e17eee644ffc2c9158971fcfd170484a838f7cdd
Sandipan Dey <hey@sandipan.dev>
2021-12-09 01:52:45 +0530

    Revert "Add name_prefix to kubemonkey message"
    This reverts commit d9667f9ccbbc9088d58a34b0a5ccaa1067bc715c.

--- modules/services/kubemonkey.tf
+++ modules/services/kubemonkey.tf
@@ -17,7 +17,6 @@ resource "helm_release" "kubemonkey" {
       timeZone : local.kubemonkey_time_zone
       whitelistedNamespaces : local.kubemonkey_whitelisted_namespaces
       notificationUrl : local.kubemonkey_notification_url
-      name_prefix : local.name_prefix
     })
   ]
 }
