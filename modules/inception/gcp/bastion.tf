9152e0b45edb90b7f957d03185ee5905b406abae
bodymindarts <justin@galoy.io>
2021-12-09 21:14:29 +0100

    Fix bitcoin version

--- modules/inception/gcp/bastion.tf
+++ modules/inception/gcp/bastion.tf
@@ -1,7 +1,7 @@
 locals {
   tag             = "${local.name_prefix}-bastion"
   cfssl_version   = "1.6.1"
-  bitcoin_version = "0.22.0"
+  bitcoin_version = "22.0"
   cepler_version  = "0.7.3"
   lnd_version     = "0.13.3"
   kubectl_version = "1.21.3"
