9cef5e3c5d436fb22c4054acb8cf1bdb56956433
bodymindarts <justin@galoy.io>
2022-01-31 09:28:57 +0100

    chore(deps): bump k8s to 1.21.6

--- modules/platform/gcp/variables.tf
+++ modules/platform/gcp/variables.tf
@@ -7,7 +7,7 @@ variable "network_prefix" {
   default = "10.1"
 }
 variable "kube_version" {
-  default = "1.21.5-gke.1302"
+  default = "1.21.6-gke.1500"
 }
 variable "node_default_machine_type" {
   default = "n2-standard-4"
