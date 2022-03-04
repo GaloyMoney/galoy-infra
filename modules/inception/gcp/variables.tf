7f691b25da6a7627868cf802a26588e2bf1f0aa3
bodymindarts <justin@galoy.io>
2022-03-04 09:59:05 +0100

    fix: platform_admin reference

--- modules/inception/gcp/variables.tf
+++ modules/inception/gcp/variables.tf
@@ -42,7 +42,7 @@ locals {
   log_viewers              = [for user in var.users : user.id if user.logs]
   inception_admins         = [for user in var.users : user.id if user.inception]
   platform_admins          = concat([for user in var.users : user.id if user.platform], ["serviceAccount:${var.inception_sa}"])
-  bastion_users            = toset(concat([for user in var.users : user.id if user.bastion], platform_admins))
+  bastion_users            = toset(concat([for user in var.users : user.id if user.bastion], local.platform_admins))
 
   region         = var.region
   network_prefix = var.network_prefix
