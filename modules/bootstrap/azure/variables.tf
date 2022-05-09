15895a3102d5ac3f600b068362b4cbbc6ba84359
Mayank Modi <fireball4556@gmail.com>
2022-05-10 03:19:33 +0530

    Create variables.tf

---
+++ modules/bootstrap/azure/variables.tf
@@ -0,0 +1,22 @@
+variable "name_prefix" {}
+variable "gcp_project" {}
+variable "organization_id" { default = "" }
+variable "external_users" { default = [] }
+variable "tf_state_bucket_location" {
+  default = "US-EAST1"
+}
+variable "tf_state_bucket_force_destroy" {
+  default = false
+}
+variable "enable_services" {
+  default = true
+}
+
+locals {
+  organization_id               = var.organization_id
+  external_users                = var.external_users
+  name_prefix                   = var.name_prefix
+  tf_state_bucket_location      = var.tf_state_bucket_location
+  tf_state_bucket_force_destroy = var.tf_state_bucket_force_destroy
+  project                       = var.gcp_project
+}
