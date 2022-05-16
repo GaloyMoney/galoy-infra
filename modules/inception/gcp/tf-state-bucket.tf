561fc2c81cd5cc0a264c817819a5d82b5a281b16
Sandipan Dey <hey@sandipan.dev>
2022-05-16 12:34:29 +0530

    feat: attach policy conditionally to tf state bucket (#42)
    * chore: attach policy conditionally to tf state bucket
    
    * feat: attach custom policy to tf state bucket
    
    * fix: variable name change
    
    * feat: list objects role attach conditionally
    
    * fix: count.index

--- modules/inception/gcp/tf-state-bucket.tf
+++ modules/inception/gcp/tf-state-bucket.tf
@@ -9,6 +9,7 @@ resource "google_storage_bucket" "tf_state" {
 }
 
 resource "google_project_iam_custom_role" "list_objects" {
+  count       = local.tf_state_bucket_policy == null ? 1 : 0
   project     = local.project
   role_id     = replace("${local.name_prefix}-objects-list", "-", "_")
   title       = "List bucket Objects"
@@ -19,6 +20,7 @@ resource "google_project_iam_custom_role" "list_objects" {
 }
 
 data "google_iam_policy" "tf_state_access" {
+  count = local.tf_state_bucket_policy == null ? 1 : 0
   binding {
     role    = "roles/storage.admin"
     members = concat(local.inception_admins, ["serviceAccount:${local.inception_sa}"])
@@ -27,7 +29,7 @@ data "google_iam_policy" "tf_state_access" {
   dynamic "binding" {
     for_each = toset(local.platform_admins)
     content {
-      role = google_project_iam_custom_role.list_objects.id
+      role = google_project_iam_custom_role.list_objects[0].id
       members = [
         binding.key
       ]
@@ -80,5 +82,5 @@ data "google_iam_policy" "tf_state_access" {
 
 resource "google_storage_bucket_iam_policy" "policy" {
   bucket      = google_storage_bucket.tf_state.name
-  policy_data = data.google_iam_policy.tf_state_access.policy_data
+  policy_data = local.tf_state_bucket_policy == null ? data.google_iam_policy.tf_state_access[0].policy_data : local.tf_state_bucket_policy
 }
