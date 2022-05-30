871337233563ddb462aca1cf9bbc7cc59515e3c6
Mayank Modi <fireball4556@gmail.com>
2022-05-31 04:37:45 +0530

    follow naming convention for storage resources

--- modules/bootstrap/azure/tf-state-storage.tf
+++ modules/bootstrap/azure/tf-state-storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "bootstrap" {
-  name                     = "${local.name_prefix}-storage-account"
+  name                     = "${local.name_prefix}StorageAcc"
   resource_group_name      = azurerm_resource_group.bootstrap.name
   location                 = local.tf_state_storage_location
   account_tier             = "Standard"
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "bootstrap" {
 }
 
 resource "azurerm_storage_container" "bootstrap" {
-  name                  = "${local.name_prefix}-storage-container"
+  name                  = "${local.name_prefix}StorageCon"
   storage_account_name  = azurerm_storage_account.bootstrap.name
   container_access_type = "private"
 }
