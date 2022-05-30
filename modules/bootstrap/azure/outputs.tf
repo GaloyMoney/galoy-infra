829f83e91abdc4ec5cf8dfa829c34b4c6090558f
Mayank Modi <fireball4556@gmail.com>
2022-05-31 05:08:27 +0530

    storage account location instead of blob storage

--- modules/bootstrap/azure/outputs.tf
+++ modules/bootstrap/azure/outputs.tf
@@ -5,5 +5,5 @@ output "tf_state_storage_name" {
     value= azurerm_storage_blob.tf_state.name
 }
 output "tf_state_storage_location" {
-    value= azurerm_storage_blob.tf_state.location
+    value= azurerm_storage_account.bootstrap.location
 }
