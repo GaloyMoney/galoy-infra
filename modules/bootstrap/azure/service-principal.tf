7920579341a6acd58784980a73c445bbc3d034f0
Mayank Modi <fireball4556@gmail.com>
2022-05-31 03:43:34 +0530

    need to use the tenant id in provider

--- modules/bootstrap/azure/service-principal.tf
+++ modules/bootstrap/azure/service-principal.tf
@@ -1,9 +1,7 @@
-# Configure Terraform
-
 # Configure the Azure Active Directory Provider
-#provider "azuread" {
-#  tenant_id = local.tenant_id
-#}
+provider "azuread" {
+  tenant_id = local.tenant_id
+}
 
 locals {
   inception_sa_name = "${local.name_prefix}-inception-tf"
@@ -11,13 +9,11 @@ locals {
 
 # Create an application
 resource "azuread_application" "inception" {
-  tenant_id = local.tenant_id
   display_name = local.inception_sa_name
 }
 
 # Create a service principal
 resource "azuread_service_principal" "bootstrap" {
-  tenant_id = local.tenant_id
   application_id = azuread_application.inception.application_id
 }
 
