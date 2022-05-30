cc4565d2e53589e1a8222b9d17b4503f51a930c6
Mayank Modi <fireball4556@gmail.com>
2022-05-31 04:25:30 +0530

    added tf state storage scripts

--- modules/bootstrap/azure/variables.tf
+++ modules/bootstrap/azure/variables.tf
@@ -1,10 +1,10 @@
 variable "name_prefix" {}
 variable "external_users" { default = [] }
 variable "tenant_id" {}
-variable "tf_state_bucket_location" {
+variable "tf_state_storage_location" {
   default = "eastus"
 }
-variable "tf_state_bucket_force_destroy" {
+variable "tf_state_storage_force_destroy" {
   default = false
 }
 variable "resource_group_location" {
@@ -16,6 +16,6 @@ locals {
   external_users                = var.external_users
   name_prefix                   = var.name_prefix
   resource_group_location		= var.resource_group_location
-  tf_state_bucket_location      = var.tf_state_bucket_location
-  tf_state_bucket_force_destroy = var.tf_state_bucket_force_destroy
+  tf_state_storage_location      = var.tf_state_storage_location
+  tf_state_storage_force_destroy = var.tf_state_storage_force_destroy
 }
