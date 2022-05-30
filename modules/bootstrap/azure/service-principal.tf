3aef66b6751b691127043ed80b293c03fafd1238
Mayank Modi <fireball4556@gmail.com>
2022-05-31 03:56:58 +0530

    added features block for azurerm

--- modules/bootstrap/azure/service-principal.tf
+++ modules/bootstrap/azure/service-principal.tf
@@ -12,6 +12,10 @@ resource "azuread_application" "inception" {
   display_name = local.inception_app_name
 }
 
+provider "azurerm" {
+  features {}
+}
+
 # Create a service principal
 resource "azuread_service_principal" "bootstrap" {
   application_id = azuread_application.inception.application_id
