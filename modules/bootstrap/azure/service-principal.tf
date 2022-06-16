d086f188ac478d63954675b4982bfdd66a2dc7a8
Mayank Modi <fireball4556@gmail.com>
2022-06-16 13:03:36 +0530

    feat: bootstrap azure module (#43)
    * Create inception-account.tf
    
    Will adapt this content to azure.
    
    * Create variables.tf
    
    * Create create_service_principle.tf
    
    * creating service principle using variable file. need to test
    
    * added variable declaration
    
    * fixing resource name in variables
    
    * create variables out of tenant id as well
    
    * removing unused code
    
    * comenting unused code
    
    * added azurerm provider info
    
    * diff provider in diff file
    
    * trying diff provider config
    
    * refactoring as per gcp standards
    
    * need to use the tenant id in provider
    
    * smol change
    
    * added features block for azurerm
    
    * added tf state storage scripts
    
    * smol change
    
    * follow naming convention for storage resources
    
    * follow naming convention for storage resources
    
    * change resource group name
    
    * need to test output.tf
    
    * storage account location instead of blob storage
    
    * fix app email output
    
    * fixed output file and tested it
    
    * formatted as per standard
    
    * format changes and exporting any names

---
+++ modules/bootstrap/azure/service-principal.tf
@@ -0,0 +1,18 @@
+# Configure the Azure Active Directory Provider
+provider "azuread" {
+  tenant_id = local.tenant_id
+}
+
+# Create an application
+resource "azuread_application" "inception" {
+  display_name = local.inception_app_name
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a service principal
+resource "azuread_service_principal" "bootstrap" {
+  application_id = azuread_application.inception.application_id
+}
