f887659d7aedcfd1c9f3093e89a4ca3cab41fb51
fireballpoint1 <fireball4556@gmail.com>
2022-07-04 00:34:28 +0530

    using vm instead of bastion host for bastion

--- modules/inception/azure/bastion.tf
+++ modules/inception/azure/bastion.tf
@@ -1,11 +1,37 @@
-resource "azurerm_bastion_host" "bastion" {
-  name                = "${local.name_prefix}-bastion"
-  location            = azurerm_resource_group.bootstrap.location
-  resource_group_name = azurerm_resource_group.bootstrap.name
+resource "azurerm_virtual_machine" "bastion" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.resource_group.location
+  resource_group_name   = azurerm_resource_group.resource_group.name
+  network_interface_ids = [azurerm_network_interface.bastion_network_interface.id]
+  vm_size               = "Standard_DS1_v2"
 
-  ip_configuration {
-    name                 = "${local.name_prefix}-bastion-ip-configuration"
-    subnet_id            = azurerm_subnet.bastionSubnet.id
-    public_ip_address_id = azurerm_public_ip.bastionIP.id
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
