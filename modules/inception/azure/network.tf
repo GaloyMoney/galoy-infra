e3f2a80630700845be0aaa427545442815c841c7
fireballpoint1 <fireball4556@gmail.com>
2022-07-04 00:34:28 +0530

    using vm instead of bastion host for bastion

--- modules/inception/azure/network.tf
+++ modules/inception/azure/network.tf
@@ -1,21 +1,25 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${local.name_prefix}-vnet"
   address_space       = ["${local.network_prefix}.0.0/16"]
-  location            = azurerm_resource_group.bootstrap.location
-  resource_group_name = azurerm_resource_group.bootstrap.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
 }
 
-resource "azurerm_subnet" "bastionSubnet" {
+resource "azurerm_subnet" "bastion_subnet" {
   name                 = "${local.name_prefix}-bastionSubnet"
-  resource_group_name  = azurerm_resource_group.bootstrap.name
+  resource_group_name  = azurerm_resource_group.resource_group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["${local.network_prefix}.0.0/24"]
+  address_prefixes     = ["${local.network_prefix}.2.0/24"]
 }
 
-resource "azurerm_public_ip" "bastionIP" {
-  name                = "${local.name_prefix}-bastionIP"
-  location            = azurerm_resource_group.bootstrap.location
-  resource_group_name = azurerm_resource_group.bootstrap.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+resource "azurerm_network_interface" "bastion_network_interface" {
+  name                = "${local.name_prefix}-nic"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  ip_configuration {
+    name                          = "bastion_network_interface_configuration1"
+    subnet_id                     = azurerm_subnet.bastion_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
