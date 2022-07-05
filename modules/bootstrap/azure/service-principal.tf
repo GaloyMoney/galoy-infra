99dffbc9eccdf22567e9f7a93711dd6bdfd8e8f5
fireballpoint1 <fireball4556@gmail.com>
2022-07-06 04:17:37 +0530

    fix spn creation for app reg

--- modules/bootstrap/azure/service-principal.tf
+++ modules/bootstrap/azure/service-principal.tf
@@ -26,7 +26,11 @@ data "azurerm_subscription" "current" {
 
 # Create Contributor role assignment for Service Principal
 resource "azurerm_role_assignment" "bootstrap_spn_contributor" {
+<<<<<<< HEAD
   scope                = data.azurerm_subscription.current.id
+=======
+  scope                = data.azurerm_subscription.current.display_name
+>>>>>>> 1ca5d9c (fix spn creation for app reg)
   role_definition_name = "Contributor"
   principal_id         = azuread_service_principal.bootstrap.id
 }
