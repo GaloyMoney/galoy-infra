418c4db46213a0823b63fdcf591e39a8a9913a0d
fireballpoint1 <fireball4556@gmail.com>
2022-07-06 20:58:07 +0530

    added providers tf to test inception module without running prep-inception (that needs to be corrected as well)

--- modules/inception/azure/network.tf
+++ modules/inception/azure/network.tf
@@ -1,21 +1,21 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${local.name_prefix}-vnet"
   address_space       = ["${local.network_prefix}.0.0/16"]
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
 }
 
 resource "azurerm_subnet" "bastion_subnet" {
   name                 = "${local.name_prefix}-bastionSubnet"
-  resource_group_name  = azurerm_resource_group.resource_group.name
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["${local.network_prefix}.2.0/24"]
 }
 
 resource "azurerm_network_interface" "bastion_network_interface" {
   name                = "${local.name_prefix}-nic"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
 
   ip_configuration {
     name                          = "bastion_network_interface_configuration1"
