dfe7b32448fa0ef839a25240b4f15d33b16a4579
Mayank Modi <fireball4556@gmail.com>
2022-07-08 16:45:14 +0530

    feat: Azure bootstrap service principle roles (#61)
    * test spn creation for running inception
    
    * fix spn creation for app reg
    
    * fix spn creation for app reg
    
    * fix spn scope arguments
    
    * longer app secret expiry
    
    * removed unused comment block

--- modules/bootstrap/azure/service-principal.tf
+++ modules/bootstrap/azure/service-principal.tf
@@ -16,3 +16,19 @@ provider "azurerm" {
 resource "azuread_service_principal" "bootstrap" {
   application_id = azuread_application.inception.application_id
 }
+
+# Create Application password (client secret)
+resource "azuread_application_password" "inception_app_password" {
+  application_object_id = azuread_application.inception.object_id
+  end_date_relative     = "48h" # expire in 3 years
+}
+
+data "azurerm_subscription" "current" {
+}
+
+# Create Contributor role assignment for Service Principal
+resource "azurerm_role_assignment" "bootstrap_spn_contributor" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.bootstrap.id
+}
