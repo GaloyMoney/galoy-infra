c82f7ae7c14b41a0ca841b1b7dc2e022f6880824
Kartik Shah <krtk.6160@gmail.com>
2022-07-28 18:31:14 +0530

    fix: enquote param to hcledit (#73)

--- ci/tasks/check-and-upgrade-k8s.sh
+++ ci/tasks/check-and-upgrade-k8s.sh
@@ -17,7 +17,7 @@ CURRENT_VERSION=$(hcledit -f modules/platform/gcp/variables.tf attribute get var
 
 if [ "$LATEST_VERSION" != "$CURRENT_VERSION" ]; then
   echo "K8s upgrade from ${CURRENT_VERSION} to ${LATEST_VERSION} is available"
-  hcledit -u -f modules/platform/gcp/variables.tf attribute set variable.kube_version.default $LATEST_VERSION
+  hcledit -u -f modules/platform/gcp/variables.tf attribute set variable.kube_version.default \"$LATEST_VERSION\"
 else
   echo "No upgrade available"
   exit 0
