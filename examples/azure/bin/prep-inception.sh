9aa21e3d390f373db60eac82c05c5356def5ec12
fireballpoint1 <fireball4556@gmail.com>
2022-08-08 19:39:57 +0530

    working on prep inception script

--- examples/azure/bin/prep-inception.sh
+++ examples/azure/bin/prep-inception.sh
@@ -6,14 +6,13 @@ pushd bootstrap
 
 terraform output > ../inception/terraform.tfvars
 
-tenant_id
-tf_state_bucket_name=$(terraform output tf_state_bucket_name | jq -r)
+tf_state_storage_account=$(terraform output tf_state_storage_account | jq -r)
+tf_state_storage_blob_name=$(terraform output tf_state_storage_blob_name | jq -r)
+tf_state_storage_container=$(terraform output tf_state_storage_container | jq -r)
+resource_group_name=$(terraform output resource_group | jq -r)
 name_prefix=$(terraform output name_prefix | jq -r)
 
 popd
-
-gcloud iam service-accounts keys create inception-sa-creds.json \
-  --iam-account=${inception_email}
 
 export ARM_CLIENT_ID = #@ data.values.testflight_azure_client_id
 export ARM_CLIENT_SECRET = #@ data.values.testflight_azure_client_secret
@@ -21,22 +20,27 @@ export ARM_CLIENT_SECRET = #@ data.values.testflight_azure_client_secret
 #export ARM_SUBSCRIPTION_ID = 
 pushd inception
 
+ACCOUNT_KEY=$(az storage account keys list --resource-group $resource_group_name --account-name $tf_state_storage_account --query '[0].value' -o tsv)
+export ARM_ACCESS_KEY=$ACCOUNT_KEY
+
 cat <<EOF > terraform.tf
 terraform {
-  backend "gcs" {
-    bucket = "${tf_state_bucket_name}"
-    prefix = "${name_prefix}/inception"
+  backend "azurerm" {
+    resource_group_name = "${resource_group_name}"
+    storage_account_name = "${tf_state_storage_account}"
+    container_name       = "${tf_state_storage_container}"
+    key                  = "terraform.tfstate"
   }
 }
 EOF
 
 terraform init
-terraform state show module.inception.google_storage_bucket.tf_state || \
-  terraform import module.inception.google_storage_bucket.tf_state ${tf_state_bucket_name}
-terraform apply \
-  -target module.inception.google_project_iam_custom_role.inception_make \
-  -target module.inception.google_project_iam_custom_role.inception_destroy \
-  -target module.inception.google_project_iam_member.inception_make \
-  -target module.inception.google_project_iam_member.inception_destroy \
-  -auto-approve
+terraform state show module.inception.azurerm_storage_account.bootstrap || \
+  terraform import module.inception.azurerm_storage_account.bootstrap ${tf_state_storage_account}
+terraform state show module.inception.azurerm_storage_container.bootstrap || \
+  terraform import module.inception.azurerm_storage_container.bootstrap ${tf_state_storage_container}
+terraform state show module.inception.azurerm_storage_blob.tf_state || \
+  terraform import module.inception.azurerm_storage_blob.tf_state ${tf_state_storage_blob_name}
+
+terraform apply 
 
---
+++ modules/inception/azure/tf-state-storage.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "bootstrap" {
+  name                     = "${local.name_prefix}sa"
+  resource_group_name      = azurerm_resource_group.bootstrap.name
+  location                 = local.tf_state_storage_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "bootstrap" {
+  name                  = "${local.name_prefix}sc"
+  storage_account_name  = azurerm_storage_account.bootstrap.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "tf_state" {
+  name                   = "${local.name_prefix}-tf-state"
+  storage_account_name   = azurerm_storage_account.bootstrap.name
+  storage_container_name = azurerm_storage_container.bootstrap.name
+  type                   = "Block"
+}
