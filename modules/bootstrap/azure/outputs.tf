b216b0d50f3f5298d832e76eab3b28a65ba59000
Sandipan Dey <hey@sandipan.dev>
2022-08-09 21:27:24 +0530

    creating vnet-subnet-publicIP and bastion host. Need to test

--- modules/bootstrap/azure/outputs.tf
+++ modules/bootstrap/azure/outputs.tf
@@ -1,19 +1,23 @@
 output "tf_state_storage_blob_name" {
   value = azurerm_storage_blob.tf_state.name
 }
+output "tf_state_storage_container" {
+  value = azurerm_storage_container.bootstrap
+}
 output "tf_state_storage_location" {
   value = azurerm_storage_account.bootstrap.location
 }
 output "tf_state_storage_account" {
   value = azurerm_storage_account.bootstrap.name
 }
-output "client_id" {
+output "resource_group" {
+  value = azurerm_resource_group.bootstrap.name
+}
+output "application_id" {
   value = azuread_application.inception.application_id
 }
 output "client_secret" {
   value     = azuread_application_password.inception_app_password.value
   sensitive = true
 }
-output "resource_group" {
-  value = azurerm_resource_group.bootstrap.name
-}
+
