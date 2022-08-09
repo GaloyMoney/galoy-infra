a492dde2aefd57659a33896ae87ec455c53f0889
fireballpoint1 <fireball4556@gmail.com>
2022-08-09 18:04:54 +0530

    fixed rebase issue in spn.tf

--- modules/bootstrap/azure/service-principal.tf
+++ modules/bootstrap/azure/service-principal.tf
@@ -23,26 +23,12 @@ resource "azuread_application_password" "inception_app_password" {
   end_date_relative     = "720h" # expire in 3 years
 }
 
-<<<<<<< HEAD
-data "azurerm_subscription" "current" {
-}
-
-=======
->>>>>>> b8e51c0 (removed unused comment block)
 data "azurerm_subscription" "current" {
 }
 
 # Create Contributor role assignment for Service Principal
 resource "azurerm_role_assignment" "bootstrap_spn_contributor" {
-<<<<<<< HEAD
-<<<<<<< HEAD
   scope                = data.azurerm_subscription.current.id
-=======
-  scope                = data.azurerm_subscription.current.display_name
->>>>>>> 1ca5d9c (fix spn creation for app reg)
-=======
-  scope                = data.azurerm_subscription.current.id
->>>>>>> 48df8e6 (fix spn scope arguments)
   role_definition_name = "Contributor"
   principal_id         = azuread_service_principal.bootstrap.id
 }
