7cf4f78256dc31dccc7f9231d43263df5c7d1bee
Mayank Modi <fireball4556@gmail.com>
2022-08-26 15:51:21 +0530

    feat: Azure module for inception (#78)
    * test spn creation for running inception
    
    * fix spn creation for app reg
    
    * fix spn creation for app reg
    
    * fix spn scope arguments
    
    * longer app secret expiry
    
    * removed unused comment block
    
    * added inception module and fixing prep inception script
    
    * inception azure module scripts
    
    * fixing main..tf
    
    * working on make bootstrap and pre inception
    
    * trying to get access key using bash script in terraform
    
    * runner upto inception is working
    
    * fix: access key json parsing
    
    * fix: make access key sensitive
    
    Co-authored-by: Sandipan Dey <hey@sandipan.dev>

--- modules/bootstrap/azure/outputs.tf
+++ modules/bootstrap/azure/outputs.tf
@@ -1,16 +1,45 @@
 output "tf_state_storage_blob_name" {
   value = azurerm_storage_blob.tf_state.name
 }
+output "tf_state_storage_container" {
+  value = azurerm_storage_container.bootstrap.name
+}
 output "tf_state_storage_location" {
   value = azurerm_storage_account.bootstrap.location
 }
 output "tf_state_storage_account" {
   value = azurerm_storage_account.bootstrap.name
 }
-output "client_id" {
+
+output "resource_group" {
+  value = azurerm_resource_group.bootstrap.name
+}
+output "application_id" {
   value = azuread_application.inception.application_id
 }
 output "client_secret" {
   value     = azuread_application_password.inception_app_password.value
+  sensitive = true
+}
+output "tenant_id" {
+  value = local.tenant_id
+}
+output "subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
+output "name_prefix" {
+  value = local.name_prefix
+}
+output "tf_state_storage_blob_id" {
+  value = azurerm_storage_blob.tf_state.id
+}
+output "tf_state_storage_container_id" {
+  value = azurerm_storage_container.bootstrap.id
+}
+output "tf_state_storage_account_id" {
+  value = azurerm_storage_account.bootstrap.id
+}
+output "tf_state_access_key" {
+  value     = data.external.access_key.result
   sensitive = true
 }
--- modules/inception/azure/tf-state-storage.tf
+++ modules/inception/azure/tf-state-storage.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "bootstrap" {
   name                     = "${local.name_prefix}sa"
-  resource_group_name      = azurerm_resource_group.bootstrap.name
+  resource_group_name      = data.azurerm_resource_group.resource_group.name
   location                 = local.tf_state_storage_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -17,13 +17,4 @@ resource "azurerm_storage_blob" "tf_state" {
   storage_account_name   = azurerm_storage_account.bootstrap.name
   storage_container_name = azurerm_storage_container.bootstrap.name
   type                   = "Block"
-}
-
-data "external" "access_key" {
-  program = ["bash", "${path.module}/tf-state-storage.sh"]
-  query = {
-    resource_group_name = azurerm_resource_group.bootstrap.name
-    storage_account     = azurerm_storage_account.bootstrap.name
-  }
-
 }
