22b7fa2833c8f4cb6eff87dd1a6568ca4398309d
Kartik Shah <krtk.6160@gmail.com>
2022-12-05 12:42:57 +0530

    chore(deps): bump otel chart to 0.40.7

--- modules/services/opentelemetry.tf
+++ modules/services/opentelemetry.tf
@@ -19,7 +19,7 @@ resource "helm_release" "otel" {
   name       = "opentelemetry-collector"
   repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
   chart      = "opentelemetry-collector"
-  version    = "0.9.1"
+  version    = "0.40.7"
   namespace  = kubernetes_namespace.otel.metadata[0].name
 
   values = [
