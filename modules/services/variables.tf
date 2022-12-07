33ba5b94f0e03c5ad9e414faa461fabcd2d5aba4
openoms <oms@tuta.io>
2022-12-07 12:48:57 +0000

    chore: remove ingress and variables

--- modules/services/variables.tf
+++ modules/services/variables.tf
@@ -14,19 +14,10 @@ variable "trace_sample_pct" {
   default = 100
 }
 
-variable "ingress_nginx_version" {
-  default = "4.0.18"
-}
-variable "cert_manager_version" {
-  default = "v1.5.3"
-}
-variable "letsencrypt_issuer_email" {}
-variable "local_deploy" { default = false }
 variable "small_footprint" { default = false }
 variable "smoketest_cronjob" { default = false }
 
 locals {
-  local_deploy             = var.local_deploy
   name_prefix              = var.name_prefix
   smoketest_namespace      = "${local.name_prefix}-smoketest"
   otel_namespace           = "${local.name_prefix}-otel"
@@ -39,13 +30,7 @@ locals {
   smoketest_cronjob_name   = "${local.smoketest_name}-cronjob"
   cluster_endpoint         = var.cluster_endpoint
   cluster_ca_cert          = var.cluster_ca_cert
-  ingress_namespace        = "${local.name_prefix}-ingress"
-  ingress_nginx_version    = var.ingress_nginx_version
-  cert_manager_version     = var.cert_manager_version
-  letsencrypt_issuer_email = var.letsencrypt_issuer_email
-  jaeger_host              = "opentelemetry-collector.${local.otel_namespace}.svc.cluster.local"
   trace_sample_pct         = var.trace_sample_pct
-  ingress_service_name     = "${var.name_prefix}-ingress"
   small_footprint          = var.small_footprint
   honeycomb_api_key        = var.honeycomb_api_key != "" ? var.honeycomb_api_key : jsondecode(var.secrets).honeycomb_api_key
 }
