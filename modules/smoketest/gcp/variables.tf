855a3c6ed18ad9bc06f45f4256b76a7a8b8e6551
Kartik Shah <krtk.6160@gmail.com>
2022-12-23 14:26:49 +0530

    chore!: extract smoketest into independent module (#116)

---
+++ modules/smoketest/gcp/variables.tf
@@ -0,0 +1,17 @@
+variable "name_prefix" {}
+variable "cluster_endpoint" {}
+variable "cluster_ca_cert" {}
+
+variable "smoketest_cronjob" { default = false }
+
+locals {
+  name_prefix      = var.name_prefix
+  cluster_endpoint = var.cluster_endpoint
+  cluster_ca_cert  = var.cluster_ca_cert
+
+  smoketest_namespace    = "${local.name_prefix}-smoketest"
+  smoketest_name         = "smoketest"
+  smoketest_cronjob      = var.smoketest_cronjob
+  smoketest_cronjob_name = "${local.smoketest_name}-cronjob"
+  galoy_namespace        = "${local.name_prefix}-galoy"
+}
