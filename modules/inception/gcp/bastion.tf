c9a9e66e1789789ea513fcfc2ba035aab07b8896
bodymindarts <justin@galoy.io>
2023-04-17 00:11:56 +0200

    chore: bump bria_version

--- modules/inception/gcp/bastion.tf
+++ modules/inception/gcp/bastion.tf
@@ -1,6 +1,6 @@
 locals {
   tag             = "${local.name_prefix}-bastion"
-  bria_version    = "0.0.6"
+  bria_version    = "0.0.7"
   cfssl_version   = "1.6.1"
   bitcoin_version = "24.0.1"
   cepler_version  = "0.7.9"
