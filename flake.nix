05ee0cc4480eea3947bd7f517eda6e922c037ea2
bodymindarts <justin@galoy.io>
2023-09-19 14:34:49 +0200

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Dev shell for galoy-infra";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; {
+        devShells.default = mkShell 
+          {
+            nativeBuildInputs = [
+              alejandra
+              terraform
+            ];
+
+            shellHook = ''
+              alias tf=terraform
+            '';
+          };
+
+        formatter = alejandra;
+      });
+}
