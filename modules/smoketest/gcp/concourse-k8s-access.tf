534ccf91ed77737c8cbb9355c46fcdfbf7882f3e
Kartik Shah <krtk.6160@gmail.com>
2024-02-06 16:28:46 +0530

    fix: add name to sa token secret

--- modules/smoketest/gcp/concourse-k8s-access.tf
+++ modules/smoketest/gcp/concourse-k8s-access.tf
@@ -43,6 +43,7 @@ resource "kubernetes_role_binding" "lnd_tls_secret_reader" {
 resource "kubernetes_secret" "concourse_k8s_access_token" {
   count = local.k8s_secret_reader_enabled ? 1 : 0
   metadata {
+    name      = "concourse-k8s-service-account-token"
     namespace = local.concourse_namespace
     annotations = {
       "kubernetes.io/service-account.name" = kubernetes_service_account.concourse[0].metadata[0].name
