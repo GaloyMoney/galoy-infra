baa894cb51d54b783abe42f05ea026d78a72f37a
Kartik Shah <krtk.6160@gmail.com>
2024-05-09 13:55:17 +0530

    chore: set backups bucket location in examples

--- examples/gcp/inception/main.tf
+++ examples/gcp/inception/main.tf
@@ -3,6 +3,11 @@ variable "tf_state_bucket_name" {}
 variable "buckets_location" {
   default = "US-EAST1"
 }
+
+variable "backups_bucket_location" {
+  default = "US-CENTRAL1"
+}
+
 variable "gcp_project" {}
 variable "inception_sa" {}
 variable "users" {
@@ -19,11 +24,12 @@ module "inception" {
   source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/inception/gcp?ref=8a373e9"
   # source = "../../../modules/inception/gcp"
 
-  name_prefix          = var.name_prefix
-  gcp_project          = var.gcp_project
-  inception_sa         = var.inception_sa
-  tf_state_bucket_name = var.tf_state_bucket_name
-  buckets_location     = var.buckets_location
+  name_prefix             = var.name_prefix
+  gcp_project             = var.gcp_project
+  inception_sa            = var.inception_sa
+  tf_state_bucket_name    = var.tf_state_bucket_name
+  buckets_location        = var.buckets_location
+  backups_bucket_location = var.backups_bucket_location
 
   users = var.users
 }
