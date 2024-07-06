806e96024981a2f8c9b8c154d2be9768f4ffad0d
Rishi Kumar <rsi.dev17@gmail.com>
2024-07-06 14:08:07 +0530

    fix: connection profile structure

--- examples/gcp/db_migration/connection_profile/main.tf
+++ examples/gcp/db_migration/connection_profile/main.tf
@@ -1,7 +1,30 @@
+variable "postgres_host" {}
+variable "postgres_user_name" {}
+variable "postgres_port" {}
+variable "postgres_password" {}
+variable "cloud_sql_id" {}
+
+resource "google_database_migration_service_connection_profile" "postgresprofile" {
+  location              = "us-east1"
+  connection_profile_id = "db-migration-connection-profile"
+  display_name          = "db-migration"
+  // don't know why I would need a label yet
+  labels = {
+    foo = "bar"
+  }
+  postgresql {
+    host         = var.postgres_host
+    port         = var.postgres_port
+    username     = var.postgres_user_name
+    password     = var.postgres_password
+    cloud_sql_id = var.cloud_sql_id
+  }
+}
+
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.36.0"
     }
   }
@@ -9,28 +32,6 @@ terraform {
 
 provider "google" {
   project = "volcano-staging"
-  region = "us-east1"
+  region  = "us-east1"
   # Configuration options
-}
-
-resource "google_database_migration_service_connection_profile" "postgresprofile" {
-  location = "us-east1"
-  connection_profile_id = "db-migration-connection-profile"
-  display_name = "db-migration"
-  labels = { 
-    foo = "bar" 
-  }
-  postgresql {
-    host = google_sql_database_instance.postgresqldb.ip_address.0.ip_address
-    port = 5432
-    username = google_sql_user.sqldb_user.name
-    password = google_sql_user.sqldb_user.password
-    ssl {
-      client_key = google_sql_ssl_cert.sql_client_cert.private_key
-      client_certificate = google_sql_ssl_cert.sql_client_cert.cert
-      ca_certificate = google_sql_ssl_cert.sql_client_cert.server_ca_cert
-    }
-    cloud_sql_id = "my-database"
-  }
-  depends_on = [google_sql_user.sqldb_user]
 }
