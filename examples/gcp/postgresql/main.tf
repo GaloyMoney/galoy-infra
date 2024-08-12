02f1b932ca0df9338576f740d577fc40b922a52b
Rishi Kumar <rsi.dev17@gmail.com>
2024-08-13 02:11:55 +0530

    test: Include all three types of db in examples

--- examples/gcp/postgresql/main.tf
+++ examples/gcp/postgresql/main.tf
@@ -5,8 +5,23 @@ variable "destroyable_postgres" {
 }
 
 module "postgresql" {
-  #source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/postgresql/gcp?ref=127af28"
-  source = "../../../modules/postgresql/gcp"
+  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/postgresql/gcp?ref=127af28"
+  # source = "../../../modules/postgresql/gcp"
+
+  instance_name          = "${var.name_prefix}-pg"
+  vpc_name               = "${var.name_prefix}-vpc"
+  gcp_project            = var.gcp_project
+  destroyable            = var.destroyable_postgres
+  user_can_create_db     = true
+  databases              = ["test"]
+  replication            = true
+  provision_read_replica = true
+  source_db_upgradable   = false
+}
+
+module "postgresql_migration_source" {
+  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/postgresql/gcp?ref=127af28"
+  # source = "../../../modules/postgresql/gcp"
 
   instance_name          = "${var.name_prefix}-pg"
   vpc_name               = "${var.name_prefix}-vpc"
@@ -20,9 +35,9 @@ module "postgresql" {
   source_db_upgradable   = true
 }
 
-module "postgresql" {
-  # source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/postgresql/gcp?ref=127af28"
-  source = "../../../modules/postgresql/gcp"
+module "postgresql_migration_destination" {
+  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/postgresql/gcp?ref=127af28"
+  # source = "../../../modules/postgresql/gcp"
 
   instance_name             = "${var.name_prefix}-pg"
   vpc_name                  = "${var.name_prefix}-vpc"
