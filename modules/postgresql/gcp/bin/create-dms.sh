0f34a50813eee58b9dd8728e34e6b9bfa580b7f8
Rishi Kumar <rsi.dev17@gmail.com>
2024-08-28 11:36:14 +0530

    fix: Reintroduce region

--- modules/postgresql/gcp/bin/create-dms.sh
+++ modules/postgresql/gcp/bin/create-dms.sh
@@ -2,12 +2,13 @@
 
 TYPE="CONTINUOUS"
 
-# Get user input for job name
+# Get user input for region and job name
+read -p "Enter the region: " REGION
 read -p "Enter the job name: " JOB_NAME
 
 # Validate user input
-if [ -z "$JOB_NAME" ]; then
-    echo "Error: Job name cannot be empty."
+if [ -z "$REGION" ] || [ -z "$JOB_NAME" ]; then
+    echo "Error: Region and job name cannot be empty."
     exit 1
 fi
 
@@ -16,8 +17,9 @@ SOURCE_ID=$(terraform output -raw source_connection_profile_id)
 DEST_ID=$(terraform output -raw destination_connection_profile_id)
 VPC=$(terraform output -raw vpc)
 
-# Construct and run the gcloud command
+# Construct and run the gcloud command to create the migration job
 gcloud database-migration migration-jobs create "$JOB_NAME" \
+    --region="$REGION" \
     --type="$TYPE" \
     --source="$SOURCE_ID" \
     --destination="$DEST_ID" \
@@ -26,11 +28,13 @@ gcloud database-migration migration-jobs create "$JOB_NAME" \
 echo "Migration job '$JOB_NAME' created successfully."
 
 # Demote the destination
-gcloud database-migration migration-jobs demote-destination "$JOB_NAME"
+gcloud database-migration migration-jobs demote-destination "$JOB_NAME" \
+    --region="$REGION"
 
 echo "Migration job '$JOB_NAME' demoted the destination successfully."
 
 # Start the DMS
-gcloud database-migration migration-jobs start "$JOB_NAME"
+gcloud database-migration migration-jobs start "$JOB_NAME" \
+    --region="$REGION"
 
 echo "Migration job '$JOB_NAME' has started."
