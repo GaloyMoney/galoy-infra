7a60052750dfc4848f8aa546a6f7257fcc762cef
Rishi Kumar <rsi.dev17@gmail.com>
2024-10-22 17:23:12 +0530

    chore: Add sanity check to postgres-perms-update.sh

--- modules/postgresql/gcp/outputs.tf
+++ modules/postgresql/gcp/outputs.tf
@@ -62,7 +62,7 @@ output "source_instance" {
 output "migration_sql_command" {
   value = local.prep_upgrade_as_source_db ? {
     destination_connection_string = "psql postgres://postgres:${module.migration[0].postgres_user_password}@${module.migration[0].destination_instance_private_ip_address}:5432/postgres"
-    alter_admin_name = "ALTER ROLE cloudsqlexternalsync RENAME TO ${google_sql_user.admin.name}"
-    alter_admin_password =  "ALTER ROLE ${google_sql_user.admin.name} PASSWORD ${random_password.admin.result}"
+    alter_admin_name              = "ALTER ROLE cloudsqlexternalsync RENAME TO ${google_sql_user.admin.name}"
+    alter_admin_password          = "ALTER ROLE ${google_sql_user.admin.name} PASSWORD ${random_password.admin.result}"
   } : {}
 }
