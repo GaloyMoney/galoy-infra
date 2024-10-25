c810aec0f6187f508369b0ea25112a76a45e0c43
Rishi Kumar <96657880+k3yss@users.noreply.github.com>
2024-10-25 14:01:59 +0100

    fix: Migration scripts and guide (#229)
    * feat: Add functionality for OUTPUT_PREFIX
    
    * chore: Introduce output name
    
    * chore: Make migration_sql_commands clear
    
    * fix: Destination db version to 16
    
    * fix: postgres-perms script
    
    * chore: Add sanity check to postgres-perms-update.sh
    
    * docs: Mention warning
    
    * chore: remove name string

--- modules/postgresql/gcp/outputs.tf
+++ modules/postgresql/gcp/outputs.tf
@@ -61,6 +61,8 @@ output "source_instance" {
 }
 output "migration_sql_command" {
   value = local.prep_upgrade_as_source_db ? {
-    sql_command = "psql postgres://postgres:${module.migration[0].postgres_user_password}@${module.migration[0].destination_instance_private_ip_address}:5432/postgres -c \"ALTER ROLE cloudsqlexternalsync RENAME TO \\\"${google_sql_user.admin.name}\\\"; ALTER ROLE \\\"${google_sql_user.admin.name}\\\" PASSWORD '${random_password.admin.result}';\""
+    destination_connection_string = "psql postgres://postgres:${module.migration[0].postgres_user_password}@${module.migration[0].destination_instance_private_ip_address}:5432/postgres"
+    alter_admin_name              = "ALTER ROLE cloudsqlexternalsync RENAME TO ${google_sql_user.admin.name}"
+    alter_admin_password          = "ALTER ROLE ${google_sql_user.admin.name} PASSWORD ${random_password.admin.result}"
   } : {}
 }
