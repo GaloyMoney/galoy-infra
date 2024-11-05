7ade4525fe0210226bc4eb4d99b352510ea3c98a
Kartik Shah <krtk.6160@gmail.com>
2024-11-05 20:42:49 +0530

    fix(examples): set backups bucket name

--- examples/gcp/inception/main.tf
+++ examples/gcp/inception/main.tf
@@ -30,6 +30,7 @@ module "inception" {
   tf_state_bucket_name    = var.tf_state_bucket_name
   buckets_location        = var.buckets_location
   backups_bucket_location = var.backups_bucket_location
+  backups_bucket_name     = "${var.name_prefix}-backups"
 
   users = var.users
 }
