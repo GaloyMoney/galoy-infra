cc3c700bd9a7e08edf482f75cf3460d2c020b7f7
bodymindarts <justin@galoy.io>
2025-02-25 21:04:39 +0100

    chore(azure): auto-scaling

--- modules/platform/azure/variables.tf
+++ modules/platform/azure/variables.tf
@@ -13,8 +13,17 @@ variable "cluster_name" {}
 variable "cluster_location" {
   default = "eastus"
 }
+variable "kube_version" {
+  default = "1.30.9"
+}
+variable "min_default_node_count" {
+  default = 1
+}
+variable "max_default_node_count" {
+  default = 3
+}
 locals {
-  subscription_id = var.subscription_id
+  subscription_id           = var.subscription_id
   name_prefix               = var.name_prefix
   network_prefix            = var.network_prefix
   resource_group_name       = var.resource_group_name
@@ -22,4 +31,7 @@ locals {
   vnet_name                 = var.vnet_name
   cluster_name              = var.cluster_name
   cluster_location          = var.cluster_location
+  kube_version              = var.kube_version
+  min_default_node_count    = var.min_default_node_count
+  max_default_node_count    = var.max_default_node_count
 }
