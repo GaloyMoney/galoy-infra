18f547067e636ed41b4406c130bb0ee694916454
Kartik Shah <krtk.6160@gmail.com>
2025-02-28 15:18:33 +0530

    ci: remove eu region from update checker

--- ci/k8s-upgrade/main.tf
+++ ci/k8s-upgrade/main.tf
@@ -3,13 +3,6 @@ locals {
   project        = "galoy-infra-testflight"
 }
 
-data "google_container_engine_versions" "euwest6" {
-  provider       = google-beta
-  location       = "europe-west6"
-  version_prefix = local.version_prefix
-  project        = local.project
-}
-
 data "google_container_engine_versions" "uscentral1" {
   provider       = google-beta
   location       = "us-central1"
@@ -26,12 +19,11 @@ data "google_container_engine_versions" "useast1" {
 
 locals {
   # Convert outputs to sets
-  euwest6_versions    = toset(data.google_container_engine_versions.euwest6.valid_master_versions)
   uscentral1_versions = toset(data.google_container_engine_versions.uscentral1.valid_master_versions)
   useast1_versions    = toset(data.google_container_engine_versions.useast1.valid_master_versions)
 
   # Find the intersection of all sets, i.e., common versions
-  common_versions = setintersection(local.euwest6_versions, local.uscentral1_versions, local.useast1_versions)
+  common_versions = setintersection(local.uscentral1_versions, local.useast1_versions)
 }
 
 output "latest_version" {
