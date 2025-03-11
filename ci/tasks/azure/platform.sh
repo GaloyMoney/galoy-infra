67bf623738172148322f9e6faea7f54ab4b6e22d
Kartik Shah <krtk.6160@gmail.com>
2025-03-11 17:59:19 +0530

    ci: add azure platform.sh task

--- ci/tasks/azure/platform.sh
+++ ci/tasks/azure/platform.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+set -eu
+
+source pipeline-tasks/ci/tasks/helpers.sh
+
+pushd repo/examples/gcp
+
+update_examples_git_ref
+
+init_kubeconfig
+init_bootstrap_azure
+
+bin/prep-inception.sh
+
+bin/prep-platform.sh
+echo yes | make platform
