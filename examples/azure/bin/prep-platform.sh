c0e10be22fd76e0d4a620de761fcb5c5436d90fe
Kartik Shah <krtk.6160@gmail.com>
2025-03-11 18:04:08 +0530

    chore: cleanup azure prep platform

--- examples/azure/bin/prep-platform.sh
+++ examples/azure/bin/prep-platform.sh
@@ -5,7 +5,6 @@ set -eu
 pushd bootstrap
 
 tf_state_storage_account=$(tofu output tf_state_storage_account | jq -r)
-subscription_id=$(tofu output subscription_id | jq -r)
 resource_group_name=$(tofu output resource_group_name | jq -r)
 tf_state_storage_container=$(tofu output tf_state_storage_container | jq -r)
 name_prefix=$(tofu output name_prefix | jq -r)
@@ -22,7 +21,6 @@ pushd platform
 cat <<EOF > terraform.tf
 terraform {
   backend "azurerm" {
-    subscription_id = "${subscription_id}"
     resource_group_name = "${resource_group_name}"
     storage_account_name = "${tf_state_storage_account}"
     container_name       = "${tf_state_storage_container}"
@@ -33,8 +31,6 @@ EOF
 
 cat <<EOF > terraform.tfvars
 name_prefix = "${name_prefix}"
-subscription_id = "${subscription_id}"
-resource_group_name = "${resource_group_name}"
 vnet_name = "${vnet_name}"
 EOF
 
