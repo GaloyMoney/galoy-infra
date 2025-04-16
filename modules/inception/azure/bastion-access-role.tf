345512e52d1cfd6eccdfd9a4967efbc0af3870ab
Kartik Shah <krtk.6160@gmail.com>
2025-04-16 21:37:40 +0530

    chore: run tofu fmt

--- modules/inception/azure/bastion-access-role.tf
+++ modules/inception/azure/bastion-access-role.tf
@@ -4,7 +4,7 @@ data "azuread_user" "bastion_users" {
 }
 
 resource "azurerm_role_assignment" "bastion_access" {
-  for_each             = toset(concat([for user in data.azuread_user.bastion_users: user.object_id], [local.inception_sp_id]))
+  for_each             = toset(concat([for user in data.azuread_user.bastion_users : user.object_id], [local.inception_sp_id]))
   scope                = azurerm_virtual_machine.bastion.id
   role_definition_name = "Virtual Machine User Login"
   principal_id         = each.value
