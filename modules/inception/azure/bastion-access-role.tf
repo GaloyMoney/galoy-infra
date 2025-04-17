a6a1f27d083b370316f8efd75f46cfa1b722707e
Kartik Shah <krtk.6160@gmail.com>
2025-04-17 17:15:26 +0530

    fix: azure bastion role assignment scope

--- modules/inception/azure/bastion-access-role.tf
+++ modules/inception/azure/bastion-access-role.tf
@@ -5,7 +5,7 @@ data "azuread_user" "bastion_users" {
 
 resource "azurerm_role_assignment" "bastion_access" {
   for_each             = toset(concat([for user in data.azuread_user.bastion_users : user.object_id], [local.inception_sp_id]))
-  scope                = azurerm_virtual_machine.bastion.id
+  scope                = data.azurerm_resource_group.resource_group.id
   role_definition_name = "Virtual Machine User Login"
   principal_id         = each.value
 }
