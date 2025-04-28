3fb9ac0ce1d3275767b99be8bdb59045b1947860
Kartik Shah <krtk.6160@gmail.com>
2025-04-28 18:46:57 +0530

    ci: fix teardown pg script

--- ci/tasks/azure/teardown-postgresql.sh
+++ ci/tasks/azure/teardown-postgresql.sh
@@ -11,11 +11,24 @@ update_examples_git_ref || true
 init_kubeconfig
 init_bootstrap_azure
 
+pushd bootstrap
+
+export ARM_CLIENT_ID=$(tofu output client_id | jq -r)
+export ARM_CLIENT_SECRET=$(tofu output client_secret | jq -r)
+
+popd
+
+az login --service-principal -u ${ARM_CLIENT_ID} -p ${ARM_CLIENT_SECRET} -t ${ARM_TENANT_ID}
+
 # Get the name prefix from bootstrap output
 name_prefix=$(cd bootstrap && tofu output name_prefix | jq -r)
 
 # Configure SSH for bastion host
 az ssh config -g ${name_prefix} -n ${name_prefix}-bastion -f ./sshconfig
 
-# Execute teardown on bastion host
-ssh -F ./sshconfig ${name_prefix}-${name_prefix}-bastion -- 'cd repo/examples/azure; echo yes | make destroy-postgresql'
+ssh -F ./sshconfig ${name_prefix}-${name_prefix}-bastion -- "cd repo/examples/azure; \
+  export ARM_CLIENT_ID=${ARM_CLIENT_ID}; \
+  export ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}; \
+  export ARM_TENANT_ID=${ARM_TENANT_ID}; \
+  export ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}; \
+  echo yes | make destroy-postgresql"
