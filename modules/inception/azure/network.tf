6a87d089ec46be2bc73a53d291c9e94a38bf8611
Kartik Shah <krtk.6160@gmail.com>
2025-05-05 18:51:51 +0530

    refactor: move pg network config to inception

--- modules/inception/azure/network.tf
+++ modules/inception/azure/network.tf
@@ -12,6 +12,59 @@ resource "azurerm_subnet" "dmz" {
   address_prefixes     = ["${local.network_prefix}.2.0/24"]
 }
 
+resource "azurerm_subnet" "postgres" {
+  name                 = "${local.name_prefix}-postgres"
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["${local.network_prefix}.3.0/24"]
+
+  delegation {
+    name = "fs"
+
+    service_delegation {
+      name    = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+resource "azurerm_network_security_group" "postgres" {
+  name                = "${local.name_prefix}-pg-nsg"
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+
+  security_rule {
+    name                       = "allow-postgres"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5432"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate the NSG with the PostgreSQL subnet
+resource "azurerm_subnet_network_security_group_association" "postgres" {
+  subnet_id                 = azurerm_subnet.postgres.id
+  network_security_group_id = azurerm_network_security_group.postgres.id
+}
+
+resource "azurerm_private_dns_zone" "postgres" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "postgres" {
+  name                  = "${local.name_prefix}-link"
+  private_dns_zone_name = azurerm_private_dns_zone.postgres.name
+  resource_group_name   = data.azurerm_resource_group.resource_group.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
+
 resource "azurerm_network_security_group" "bastion_security_group" {
   name                = "bastion-nsg"
   location            = data.azurerm_resource_group.resource_group.location
