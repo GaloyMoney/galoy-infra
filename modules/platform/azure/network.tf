b649169daa2b05662f84098b0ff3ef351ee5f4fb
Kartik Shah <krtk.6160@gmail.com>
2025-05-08 12:41:54 +0530

    fix: azure vnet reference

--- modules/platform/azure/network.tf
+++ modules/platform/azure/network.tf
@@ -6,6 +6,6 @@ data "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "cluster" {
   name                 = "${local.name_prefix}-cluster"
   resource_group_name  = data.azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
   address_prefixes     = ["${local.network_prefix}.0.0/17"]
 }
