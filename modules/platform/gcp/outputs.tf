8352bba5ae8a8e75e701f72d64a7eed5c6a0536c
Kartik Shah <krtk.6160@gmail.com>
2025-05-12 13:27:22 +0530

    chore!: remove LND related config (#279)
    * chore!: remove LND related config
    
    * chore: run tofu fmt

--- modules/platform/gcp/outputs.tf
+++ modules/platform/gcp/outputs.tf
@@ -13,19 +13,3 @@ output "cluster_name" {
 output "cluster_location" {
   value = google_container_cluster.primary.location
 }
-
-output "lnd1_ip" {
-  value = local.deploy_lnd_ips ? google_compute_address.lnd1[0].address : ""
-}
-
-output "lnd2_ip" {
-  value = local.deploy_lnd_ips ? google_compute_address.lnd2[0].address : ""
-}
-
-output "lnd1_internal_ip" {
-  value = local.deploy_lnd_ips ? google_compute_address.lnd1_internal_ip[0].address : ""
-}
-
-output "lnd2_internal_ip" {
-  value = local.deploy_lnd_ips ? google_compute_address.lnd2_internal_ip[0].address : ""
-}
--- modules/smoketest/gcp/concourse-k8s-access.tf
+++ modules/smoketest/gcp/concourse-k8s-access.tf
@@ -6,40 +6,6 @@ resource "kubernetes_service_account" "concourse" {
   }
 }
 
-resource "kubernetes_role" "lnd_tls_secret_reader" {
-  count = local.k8s_secret_reader_enabled ? 1 : 0
-  metadata {
-    name      = "lnd-tls-secret-reader"
-    namespace = local.bitcoin_namespace
-  }
-
-  rule {
-    api_groups = [""]
-    resources  = ["secrets"]
-    verbs      = ["get", "list"]
-  }
-}
-
-resource "kubernetes_role_binding" "lnd_tls_secret_reader" {
-  count = local.k8s_secret_reader_enabled ? 1 : 0
-  metadata {
-    name      = "lnd-tls-secret-reader"
-    namespace = local.bitcoin_namespace
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "Role"
-    name      = kubernetes_role.lnd_tls_secret_reader[0].metadata[0].name
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.concourse[0].metadata[0].name
-    namespace = local.concourse_namespace
-  }
-}
-
 resource "kubernetes_secret" "concourse_k8s_access_token" {
   count = local.k8s_secret_reader_enabled ? 1 : 0
   metadata {
