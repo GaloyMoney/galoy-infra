1c0684deb510e8afbfe69397810f1cf953998f21
Kartik Shah <krtk.6160@gmail.com>
2025-05-12 19:09:27 +0530

    fix: azure smoketest provider config

--- examples/azure/smoketest/main.tf
+++ examples/azure/smoketest/main.tf
@@ -6,24 +6,24 @@ variable "name_prefix" {}
 variable "cluster_endpoint" {}
 variable "cluster_ca_cert" {}
 
-data "google_client_config" "default" {}
+data "azurerm_client_config" "current" {}
 
 provider "kubernetes" {
   host                   = var.cluster_endpoint
-  token                  = data.google_client_config.default.access_token
+  token                  = data.azurerm_client_config.current.access_token
   cluster_ca_certificate = var.cluster_ca_cert
 }
 
 provider "helm" {
   kubernetes {
     host                   = var.cluster_endpoint
-    token                  = data.google_client_config.default.access_token
+    token                  = data.azurerm_client_config.current.access_token
     cluster_ca_certificate = var.cluster_ca_cert
   }
 }
 
 module "smoketest" {
-  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/smoketest?ref=0e25c76"
+  source = "git::https://github.com/GaloyMoney/galoy-infra.git//modules/smoketest?ref=7005338"
   # source = "../../../modules/smoketest"
 
   name_prefix      = var.name_prefix
