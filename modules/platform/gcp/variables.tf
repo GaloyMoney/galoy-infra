072fc78ddd7988f2b5a6a4c8634ad6e0713265ff
k9ert <117085+k9ert@users.noreply.github.com>
2025-07-01 14:44:10 +0200

    fix: use supported version

--- modules/platform/gcp/variables.tf
+++ modules/platform/gcp/variables.tf
@@ -10,7 +10,7 @@ variable "network_prefix" {
   default = "10.1"
 }
 variable "kube_version" {
-  default = "1.31.8-gke.1045000"
+  default = "1.31.8-gke.1113000"
 }
 variable "node_default_machine_type" {
   default = "n2-standard-4"
