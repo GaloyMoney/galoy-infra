aadd4814f82ec2261b09c013a9934f3bf54b04a6
Kartik Shah <krtk.6160@gmail.com>
2025-08-28 16:18:00 +0530

    fix: default k8s version in platform (#296)

--- modules/platform/gcp/variables.tf
+++ modules/platform/gcp/variables.tf
@@ -10,7 +10,7 @@ variable "network_prefix" {
   default = "10.1"
 }
 variable "kube_version" {
-  default = "null"
+  default = "1.30.14-gke.1059000"
 }
 variable "node_default_machine_type" {
   default = "n2-standard-4"
