cd36b5268bf33708922f4d329b4304a412f0c540
Nicolas Burtey <nb@galoy.io>
2026-05-25 18:56:11 +0200

    fix: use v1 kubernetes smoketest resources

--- modules/smoketest/main.tf
+++ modules/smoketest/main.tf
@@ -1,13 +1,13 @@
-resource "kubernetes_namespace" "smoketest" {
+resource "kubernetes_namespace_v1" "smoketest" {
   metadata {
     name = local.smoketest_namespace
   }
 }
 
-resource "kubernetes_role" "smoketest" {
+resource "kubernetes_role_v1" "smoketest" {
   metadata {
     name      = local.smoketest_name
-    namespace = kubernetes_namespace.smoketest.metadata[0].name
+    namespace = kubernetes_namespace_v1.smoketest.metadata[0].name
   }
 
   rule {
@@ -17,47 +17,47 @@ resource "kubernetes_role" "smoketest" {
   }
 }
 
-resource "kubernetes_service_account" "smoketest" {
+resource "kubernetes_service_account_v1" "smoketest" {
   metadata {
     name      = local.smoketest_name
-    namespace = kubernetes_namespace.smoketest.metadata[0].name
+    namespace = kubernetes_namespace_v1.smoketest.metadata[0].name
   }
 }
 
-resource "kubernetes_secret" "smoketest_token" {
+resource "kubernetes_secret_v1" "smoketest_token" {
   metadata {
     name      = "${local.smoketest_name}-token"
-    namespace = kubernetes_namespace.smoketest.metadata[0].name
+    namespace = kubernetes_namespace_v1.smoketest.metadata[0].name
     annotations = {
-      "kubernetes.io/service-account.name" = kubernetes_service_account.smoketest.metadata[0].name
+      "kubernetes.io/service-account.name" = kubernetes_service_account_v1.smoketest.metadata[0].name
     }
   }
 
   type = "kubernetes.io/service-account-token"
 }
 
-data "kubernetes_secret" "smoketest_token" {
+data "kubernetes_secret_v1" "smoketest_token" {
   metadata {
-    name      = kubernetes_secret.smoketest_token.metadata[0].name
-    namespace = kubernetes_namespace.smoketest.metadata[0].name
+    name      = kubernetes_secret_v1.smoketest_token.metadata[0].name
+    namespace = kubernetes_namespace_v1.smoketest.metadata[0].name
   }
 }
 
-resource "kubernetes_role_binding" "smoketest" {
+resource "kubernetes_role_binding_v1" "smoketest" {
   metadata {
     name      = local.smoketest_name
-    namespace = kubernetes_namespace.smoketest.metadata[0].name
+    namespace = kubernetes_namespace_v1.smoketest.metadata[0].name
   }
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
-    name      = kubernetes_role.smoketest.metadata[0].name
+    name      = kubernetes_role_v1.smoketest.metadata[0].name
   }
 
   subject {
     kind      = "ServiceAccount"
     name      = local.smoketest_name
-    namespace = kubernetes_role.smoketest.metadata[0].namespace
+    namespace = kubernetes_role_v1.smoketest.metadata[0].namespace
   }
 }
