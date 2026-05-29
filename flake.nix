826c0d86094a143f6a3f57d85f300a5418e294aa
Kartik Shah <krtk.6160@gmail.com>
2026-05-29 16:13:55 +0530

    fix(ci): build and use tofu resource image (#340)
    * fix(ci): use galoy opentofu resource
    
    * fix(ci): update opentofu runtime
    
    * fix(ci): build tofu resource image

--- flake.nix
+++ flake.nix
@@ -3,18 +3,76 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+      pkgsUnstable = import nixpkgs-unstable {inherit system;};
+      terraformResourceSrc = pkgs.fetchFromGitHub {
+        owner = "ljfranklin";
+        repo = "terraform-resource";
+        rev = "2eba5a46e96587d0eafcee857ee8591e9e9a8b01";
+        hash = "sha256-wSzG/2nC4BPvnLZ4HA2gxYBt9+xTA2KAOVt/1rbAwIQ=";
+      };
+      tofuResource = pkgs.buildGoModule {
+        pname = "tofu-resource";
+        version = "2026-05-29";
+        src = "${terraformResourceSrc}/src/terraform-resource";
+        vendorHash = null;
+        subPackages = [
+          "cmd/check"
+          "cmd/in"
+          "cmd/out"
+        ];
+        postInstall = ''
+          mkdir -p $out/opt/resource
+          mv $out/bin/check $out/opt/resource/check
+          mv $out/bin/in $out/opt/resource/in
+          mv $out/bin/out $out/opt/resource/out
+        '';
+      };
+      tofuResourceImage = pkgs.dockerTools.streamLayeredImage {
+        name = "us.gcr.io/galoyorg/tofu-resource";
+        tag = "latest";
+        architecture = "amd64";
+        maxLayers = 4;
+        contents = [
+          tofuResource
+          pkgsUnstable.opentofu
+          pkgs.bash
+          pkgs.cacert
+          pkgs.coreutils
+          pkgs.curl
+          pkgs.git
+          pkgs.gnugrep
+          pkgs.gnused
+          pkgs.openssh
+          pkgs.unzip
+        ];
+        extraCommands = ''
+          mkdir -p usr/local/bin root/.ssh
+          ln -s /bin/tofu usr/local/bin/terraform
+          printf '%s\n' 'StrictHostKeyChecking no' 'LogLevel quiet' > root/.ssh/config
+          chmod 0600 root/.ssh/config
+        '';
+        config = {
+          Env = ["PATH=/usr/local/bin:/bin"];
+        };
+      };
     in
       with pkgs; {
+        packages = {
+          tofu-resource-image = tofuResourceImage;
+        };
+
         devShells.default = mkShell
           {
             nativeBuildInputs = [
